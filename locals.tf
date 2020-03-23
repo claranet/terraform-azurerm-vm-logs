51b768a8d4a7b4d2728be3cd6e4360b10f186116
Rémi Beaufils <remi.beaufils@fr.clara.net>
2020-03-23 14:00:01 +0100

    AZ-206: pin azurerm version under 2.0 and manage installation of GPG package on VMs

--- locals.tf
+++ locals.tf
@@ -3,4 +3,8 @@ locals {
     env   = var.environment
     stack = var.stack
   }
+
+  settings_linux = {
+    commandToExecute = "[[ $(awk -F'=' '/ID_LIKE/ { print $2 }' /etc/os-release 2>/dev/null) == 'debian' ]] && apt update && apt install -y gpg || true"
+  }
 }
