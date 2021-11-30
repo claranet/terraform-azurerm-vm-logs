e6e8ccd9270241189d054b5a9b7232c1e9773e9a
Jérôme Respaut <jerome.respaut@fr.clara.net>
2021-11-30 16:52:34 +0100

    AZ-572: Update example with latest version of `linux-vm` module

--- examples/main/modules.tf
+++ examples/main/modules.tf
@@ -58,17 +58,42 @@ module "azure_network_subnet" {
   subnet_cidr_list     = ["10.10.10.0/24"]
 }
 
+module "az_monitor" {
+  source  = "claranet/run-iaas/azurerm//modules/vm-monitoring"
+  version = "x.x.x"
+
+  client_name    = var.client_name
+  location       = module.azure_region.location
+  location_short = module.azure_region.location_short
+  environment    = var.environment
+  stack          = var.stack
+
+  resource_group_name        = module.rg.resource_group_name
+  log_analytics_workspace_id = module.logs.log_analytics_workspace_id
+
+  extra_tags = {
+    foo = "bar"
+  }
+}
+
+resource "azurerm_availability_set" "vm_avset" {
+  name                = "${var.stack}-${var.client_name}-${module.azure_region.location_short}-${var.environment}-as"
+  location            = module.azure_region.location
+  resource_group_name = module.rg.resource_group_name
+  managed             = true
+}
+
 module "vm" {
   source  = "claranet/linux-vm/azurerm"
   version = "x.x.x"
 
-  location       = module.azure_region.location
-  location_short = module.azure_region.location_short
-  client_name    = var.client_name
-  environment    = var.environment
-  stack          = var.stack
-
+  location            = module.azure_region.location
+  location_short      = module.azure_region.location_short
+  client_name         = var.client_name
+  environment         = var.environment
+  stack               = var.stack
   resource_group_name = module.rg.resource_group_name
+
 
   subnet_id      = module.azure_network_subnet.subnet_id
   vm_size        = "Standard_B2s"
@@ -76,7 +101,15 @@ module "vm" {
   admin_username = var.vm_administrator_login
   ssh_public_key = var.ssh_public_key
 
-  zone_id = 1
+  diagnostics_storage_account_name      = module.logs.logs_storage_account_name
+  diagnostics_storage_account_sas_token = null # used by legacy agent only
+  azure_monitor_data_collection_rule_id = module.az_monitor.data_collection_rule_id
+  log_analytics_workspace_guid          = module.logs.log_analytics_workspace_guid
+  log_analytics_workspace_key           = module.logs.log_analytics_workspace_primary_key
+
+  availability_set_id = azurerm_availability_set.vm_avset.id
+  # or use Availability Zone
+  # zone_id = 1
 
   vm_image = {
     publisher = "Debian"
@@ -85,8 +118,26 @@ module "vm" {
     version   = "latest"
   }
 
-  diagnostics_storage_account_name      = module.logs.logs_storage_account_name
-  diagnostics_storage_account_sas_token = module.logs.logs_storage_account_sas_token
+  storage_data_disk_config = {
+    appli_data_disk = {
+      name                 = "appli_data_disk"
+      disk_size_gb         = 512
+      lun                  = 0
+      storage_account_type = "Standard_LRS"
+      extra_tags = {
+        some_data_disk_tag = "some_data_disk_tag_value"
+      }
+    }
+    logs_disk = {
+      # Used to define Logical Unit Number (LUN) parameter
+      lun          = 10
+      disk_size_gb = 64
+      caching      = "ReadWrite"
+      extra_tags = {
+        some_data_disk_tag = "some_data_disk_tag_value"
+      }
+    }
+  }
 }
 
 module "vm_logs" {
